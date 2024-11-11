fab7edff561be1ebb1ef2de72ea561995def0c03
williammhn <williammhn7@gmail.com>
2024-11-11 16:27:24 +0200

    Security commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import SearchBar from './SearchBar.jsx'
 import NYT from './NYTapi.jsx'
 import './App.css'
 
-// Exemple call : https://api.nytimes.com/svc/archive/v1/2024/1.json?api-key=F5QaAZeSsncE5ix4h6ZTy66HApHlAzAs
-
 
 function App() {
 
